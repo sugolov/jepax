00d8bbab15cc773084c0d4b802fdf210231525b0
anton <asugolov@gmail.com>
2026-01-24 00:29:15 -0500

    data

--- jepax/data/dataset.py
+++ jepax/data/dataset.py
@@ -168,9 +168,11 @@ def build_dataloader(
 
     if sharding:
         num_processes = jax.process_count()
+        print(f"num processes: {num_processes}")
         assert batch_size % num_processes == 0, "The batch size must divide number "\
             "of processes"
         batch_size_loader = batch_size // num_processes
+        print(f"batch size loader: {batch_size_loader}")
     else:
         batch_size_loader = batch_size
 
